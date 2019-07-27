e5cf99bd2d230a811ecfb9ce6beb450bd396643d
youngMen1 <fengzhiqiangzxx199644>
2019-07-27 17:13:28 +0800

    desc:spring4.0学习

---
+++ spring-four/src/main/java/com/seal/four/springfour/SpringFourApplication.java
@@ -0,0 +1,13 @@
+package com.seal.four.springfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFourApplication.class, args);
+    }
+
+}
---
+++ spring-four/src/test/java/com/seal/four/springfour/SpringFourApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seal.four.springfour;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFourApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
