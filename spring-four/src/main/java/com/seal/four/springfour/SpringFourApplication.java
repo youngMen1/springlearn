93053a81988cb8b11a8806de886c2cd36ded146b
youngMen1 <fengzhiqiangzxx199644>
2019-07-31 11:21:06 +0800

    desc:注释

--- spring-four/src/main/java/com/seal/four/springfour/SpringFourApplication.java
+++ spring-four/src/main/java/com/seal/four/springfour/SpringFourApplication.java
@@ -2,7 +2,12 @@ package com.seal.four.springfour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author zhiqiang.feng
+ * @version 1.0c
+ * @date-time 2019/3/15 16:00
+ * @description spring学习
+ **/
 @SpringBootApplication
 public class SpringFourApplication {
 
